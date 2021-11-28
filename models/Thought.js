eb52d4f218e316a06d7a8413e6ff8bd3c6203c51
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-11-28 00:37:14 -0700

    Thought model

---
+++ models/Thought.js
@@ -0,0 +1,77 @@
+const { Schema, model, Types } = require('mongoose');
+
+const { formatDate } = require();
+const { User } = require('./User');
+
+
+
+const ReactionSchema = new Schema({
+
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: 'Please leave a reaction!',
+    maxLength: 280
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => formatDate(createdAtVal)
+  }
+},
+  {
+    toJSON: {
+      getters: true
+    }
+  });
+
+
+
+
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => formatDate(createdAtVal, 'dd/MM/yyyy hh:mm')
+  },
+  username:
+  {
+    type: String,
+    required: true,
+    ref: 'User'
+  },
+  reactions: [ReactionSchema]
+},
+  
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+ 
+ThoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+})
+
+
+const Thought = model('Thought', ThoughtSchema);
+
+
+module.exports = Thought;
