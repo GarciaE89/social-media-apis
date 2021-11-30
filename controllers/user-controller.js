7f320f0eee74d2d2930ded42a54d96ddd7baf712
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-11-29 21:50:14 -0700

    updated user controller with correct params on .then

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -11,11 +11,12 @@ const thoughtControllers = {
           })
           .select('-__v')
           .sort({ _id: -1 })
-          .then(dbPizzaData => res.json(dbPizzaData))
+          .then(dbThoughtsData => res.json(dbThoughtsData))
           .catch(err => {
             console.log(err);
             res.status(400).json(err);
           });
       },
 
-}
+},
+
